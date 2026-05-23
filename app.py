4bad36a344b8ca87a89f67de912b33372f7782f1
arkanhanif361 <m.arkanhanif18@gmail.com>
2026-05-23 10:18:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.set_page_config(
   page_title="Dunia_Praktisi_2025",
   page_icon="🧊",
-  layout="centered"
+  layout="centered",
+  initial_sidebar_state="expanded",
 )
 
 
@@ -11,3 +12,4 @@ st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
 st.caption("Made with ❤️ using Streamlit")
+st.write("Hello, *World!* :sunglasses:")
